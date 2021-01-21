12eb2de90f68c5ccd331609e19c3d5cc96b1d275
Hiroki Ikeuchi <ikeuchi.hiroki@jp.panasonic.com>
2021-01-21 18:14:29 +0900

    fix calcOdom tf->tf2

--- scripts/sim_with_judge.sh
+++ scripts/sim_with_judge.sh
@@ -2,6 +2,16 @@
 set -e
 set -x
 
+# check arg num
+if [ $# -ne 2 ]; then
+    RED_NAME="you"
+    BLUE_NAME="enemy"
+else
+    RED_NAME=$1
+    BLUE_NAME=$2
+fi
+
+
 # judge
 # run judge server and visualize window
 gnome-terminal -- python judge/judgeServer.py
@@ -10,7 +20,7 @@ sleep 1
 gnome-terminal -- python judge/JudgeWindow.py
 
 # init judge server for sim setting
-bash judge/test_scripts/init_single_play.sh judge/marker_set/sim.csv localhost:5000 you enemy
+bash judge/test_scripts/init_single_play.sh judge/marker_set/sim.csv localhost:5000  $RED_NAME $BLUE_NAME
 
 # robot
 roslaunch burger_war setup_sim.launch
