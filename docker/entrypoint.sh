3a4e3f99e2e812617dea521bc797813dde26a800
Kotaro Inoue <k.inoue@effect-effect.com>
2021-01-22 12:00:08 +0900

    UID違いのユーザー対応

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,12 +4,15 @@ set -e
 # DEVELOPER_NAMEはdocker build時に置き換える
 DEVELOPER_NAME=developer
 
-# ホストPCとUSER ID/GROUP IDを合わせる(ファイルアクセスできなくなる為)
-sudo usermod -u ${HOST_USER_ID} -o -m ${DEVELOPER_NAME} -d /home/${DEVELOPER_NAME}
-sudo groupmod -g ${HOST_GROUP_ID} ${DEVELOPER_NAME}
+if [ "$(id -u)" == "0" ]; then
 
-# setup ros environment
-# source "${HOME}/.bashrc"
-# source "/opt/ros/$ROS_DISTRO/setup.bash"
-# exec /bin/bash -l -c "$*"
-exec "$@"
+  if [ "${HOST_USER_ID}" != "$(gosu ${DEVELOPER_NAME} id -u)" ]; then
+    # ホストPCとUSER ID/GROUP IDを合わせる(ファイルアクセスできなくなる為)
+    usermod -u ${HOST_USER_ID} -o -m -d /home/${DEVELOPER_NAME} ${DEVELOPER_NAME}
+    groupmod -g ${HOST_GROUP_ID} ${DEVELOPER_NAME}
+    chown -R ${DEVELOPER_NAME}:${DEVELOPER_NAME} /home/${DEVELOPER_NAME}
+  fi
+  su - ${DEVELOPER_NAME}
+fi
+
+exec gosu ${DEVELOPER_NAME} "$@" 
