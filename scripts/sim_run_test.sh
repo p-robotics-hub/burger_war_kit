549e3d14cb854ea16485a3f33cf247bfdfce0047
Kotaro Inoue <k.inoue@effect-effect.com>
2021-02-26 17:35:29 +0900

    自動テスト対応

--- scripts/sim_run_test.sh
+++ scripts/sim_run_test.sh
@@ -26,12 +26,15 @@ LOG_ARHCIVE_NAME=test_logs
 # ログ出力先
 LOG_ROOT_DIR=${HOME}/catkin_ws/logs
 TEST_LOG_DIR=${LOG_ROOT_DIR}/test
-SIM_JUDGE_LOG="${TEST_LOG_DIR}/sim_with_judge_nogui.log"
+SCREENSHOT_DIR=${TEST_LOG_DIR}/screenshot
+SIM_JUDGE_LOG="${TEST_LOG_DIR}/sim_with_test.log"
 SIM_START_LOG="${TEST_LOG_DIR}/start_test.log"
+JUDGE_SERVER_RESULT_LOG="${TEST_LOG_DIR}/judge_server_result.log"
 
 # テスト結果初期化
 [ -d "${TEST_LOG_DIR}" ] || mkdir -p "${TEST_LOG_DIR}"
 rm -rf ${TEST_LOG_DIR}/*
+[ -d "${SCREENSHOT_DIR}" ] || mkdir -p "${SCREENSHOT_DIR}"
 
 # 接続するJudgeServerのURL
 JUDGE_SERVER_ADDR=http://localhost:5000/warState
@@ -113,7 +116,7 @@ echo "==========================================================================
 echo " Simulator Startup"
 echo "==============================================================================="
 # シミュレータ起動
-(bash ${SCRIPT_DIR}/sim_with_judge_nogui.sh > "${SIM_JUDGE_LOG}" 2>&1) &
+(bash ${SCRIPT_DIR}/sim_with_test.sh > "${SIM_JUDGE_LOG}" 2>&1) &
 SIM_JUDGE_PID=$!
 
 # judgeサーバーが立ち上がるまでの仮待機(時間は要調整)
@@ -146,23 +149,38 @@ SIM_START_PID=$!
 sleep 1
 
 # シミュレーション終了待ち
-TIMEOUT_SECOND=300
-while [ ${TIMEOUT_SECOND} -ne 0 ]
+TIMEOUT_SECOND=600
+while [ ${TIMEOUT_SECOND} -gt 0 ]
 do
+  # ループ周期
+  sleep 10 &
+  sleep_id=$!
+  # 標準出力へのログ出力
   curl -s ${JUDGE_SERVER_ADDR} | jq -c '. | { time:.time, state:.state, ready:.ready, scores:.scores }'
-  sleep 1
+  real_time=$(curl -s ${JUDGE_SERVER_ADDR} | jq '.time' | sed 's/\(.*\)\..*$/\1/')
+  sim_time=$(rostopic echo -n 1 /clock| sed -n '/\ssecs:/s/^.*:\s*\(.*\)$/\1/p')
+  echo "SIM TIME: ${sim_time} sec"
+  # 画面キャプチャ
+  screenshot_file=$(printf %03d ${sim_time})sec.png
+  scrot ${SCREENSHOT_DIR}/${screenshot_file}
   if ! ( curl -s ${JUDGE_SERVER_ADDR} | jq .state | grep -c 'running' > /dev/null ) ; then
-    # シミュレーション状態が実施中以外になった場合、終了
+    # シミュレーション状態が実施中以外になった場合
+    break
+  elif [ ${sim_time} -ge 180 -a ${real_time} -ge 180 ]; then
+    # GazeboのSim TimeとJudgeServerの試合時間が３分経過した場合
     break
   fi
-  TIMEOUT_SECOND=$((TIMEOUT_SECOND - 1))
+  TIMEOUT_SECOND=$((TIMEOUT_SECOND - 10))
+  # 待機
+  wait ${sleep_id}
 done
 
 # 終了処理
 #------------------------------------------------
 # 得点取得
-BLUE_POINT=$( curl -s ${JUDGE_SERVER_ADDR} | jq .scores.b )
-RED_POINT=$( curl -s ${JUDGE_SERVER_ADDR} | jq .scores.r )
+curl -s ${JUDGE_SERVER_ADDR} > ${JUDGE_SERVER_RESULT_LOG}
+BLUE_POINT=$( cat ${JUDGE_SERVER_RESULT_LOG} | jq .scores.b )
+RED_POINT=$( cat ${JUDGE_SERVER_RESULT_LOG} | jq .scores.r )
 
 # テスト結果確認
 TEST_RESULT=0
@@ -173,18 +191,13 @@ if ! echo "${RED_POINT}" | grep -q -E "^[123456789]"; then
   TEST_RESULT=1
 fi
 
-RESULT_MESSAGE="\e[32mPASSED.\e[m"
-if [ ${TEST_RESULT} -ne 0 ]; then
-  RESULT_MESSAGE="\e[31mFAILED...\e[m"
-fi
-
 # 子プロセスを落とす
 killpstree ${SIM_START_PID}
 killpstree ${SIM_JUDGE_PID}
 
 # 本プロセスが早く落ちすぎて子プロセスがゾンビ化するため一定時間待機する
 echo "Wait simulator shutdown ..."
-sleep 15
+sleep 30
 
 # ログを集める
 cp -r "${HOME}/.ros/log" "${TEST_LOG_DIR}/ros"
@@ -193,6 +206,10 @@ cp -r "${HOME}/catkin_ws/src/burger_war_kit/judge/log" "${TEST_LOG_DIR}/judge"
 tar czvf "${LOG_ROOT_DIR}/${LOG_ARHCIVE_NAME}.tgz" -C "${LOG_ROOT_DIR}" ./test
 
 # 終了時のメッセージを出力
+RESULT_MESSAGE="\e[32mPASSED.\e[m"
+if [ ${TEST_RESULT} -ne 0 ]; then
+  RESULT_MESSAGE="\e[31mFAILED...\e[m"
+fi
 echo "==============================================================================="
 echo -e " TEST RESULT: ${RESULT_MESSAGE}"
 echo "-------------------------------------------------------------------------------"
