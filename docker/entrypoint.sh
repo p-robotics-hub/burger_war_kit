56be868d75b8dc1280e513f0a17b7cbe029782ec
Kotaro Inoue <k.inoue@effect-effect.com>
2021-03-08 12:35:15 +0900

    sleep追加

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,6 +14,7 @@ if [ "$(id -u)" == "0" ]; then
   if [ "${HOST_USER_ID}" != "$(gosu ${DEVELOPER_NAME} id -u)" ]; then
     # ホストPCとUSER ID/GROUP IDを合わせる(ファイルアクセスできなくなる為)
     usermod -u ${HOST_USER_ID} -o -m -d /home/${DEVELOPER_NAME} ${DEVELOPER_NAME}
+    sleep 5
     groupmod -g ${HOST_GROUP_ID} ${DEVELOPER_NAME}
     chown -R ${DEVELOPER_NAME}:${DEVELOPER_NAME} /home/${DEVELOPER_NAME}
   fi
