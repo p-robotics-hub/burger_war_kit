c6bd8b105b6a763072f83586be317b8fe8cdbde2
Kotaro Inoue <k.inoue@effect-effect.com>
2021-03-08 11:40:55 +0900

    update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,23 +5,19 @@ set -x
 # DEVELOPER_NAMEはdocker build時に置き換える
 DEVELOPER_NAME=developer
 
-echo "HOST_USER_ID: ${HOST_USER_ID}"
-echo "HOST_GROUP_ID: ${HOST_GROUP_ID}"
-echo "RUN USER ID: $(id)"
+echo RUN ID: $(id)
+echo HOST_USER_ID: ${HOST_USER_ID}
+echo HOST_GROUP_ID: ${HOST_GROUP_ID}
 
 if [ "$(id -u)" == "0" ]; then
-
-  echo "DEVELOPER ID: $(gosu ${DEVELOPER_NAME} id)"
 
   if [ "${HOST_USER_ID}" != "$(gosu ${DEVELOPER_NAME} id -u)" ]; then
     # ホストPCとUSER ID/GROUP IDを合わせる(ファイルアクセスできなくなる為)
     usermod -u ${HOST_USER_ID} -o -m -d /home/${DEVELOPER_NAME} ${DEVELOPER_NAME}
     groupmod -g ${HOST_GROUP_ID} ${DEVELOPER_NAME}
-    chown -R ${DEVELOPER_NAME}:${DEVELOPER_NAME} /home/${DEVELOPER_NAME} /home/${DEVELOPER_NAME}/.*
+    chown -R ${DEVELOPER_NAME}:${DEVELOPER_NAME} /home/${DEVELOPER_NAME}
   fi
-
   su - ${DEVELOPER_NAME}
-  echo "DEVELOPER ID4: $(gosu ${DEVELOPER_NAME} id)"
 fi
 
-exec gosu ${DEVELOPER_NAME} "$@" 
+exec gosu ${DEVELOPER_NAME} "$@"
